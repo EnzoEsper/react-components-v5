3d45b1d00f56b7a7933ea6ee516728ad882f831c
Firstname Lastname <enzoesper7@gmail.com>
2023-07-18 16:25:34 -0300

    adding test button and updating package.json

---
+++ src/components/TestButton/TestButton.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@mui/material'
+
+const TestButton = () => {
+  return <Button>TestButton</Button>
+}
+
+export default TestButton
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // export { default as MUIButton } from './stories/MUIButton'
 // export { default as Chip } from './stories/Chip'
 // export { default as TestChip } from './components/TestChip'
-import TestChip from './components/TestChip/TestChip'
+import TestChip from './components/TestChip'
+import TestButton from './components/TestButton'
 
-export { TestChip }
+export { TestChip, TestButton }
